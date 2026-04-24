2ac9ca62e5e2217495c7fe5bac3ea52ad6253e62
Yana Bondarenko <yana-lyana27@mail.ru>
2026-04-24 21:26:42 +0200

    fix busg in app

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,26 +1,3 @@
-/* 
-import { fetchNoteById } from "@/lib/api";
-
-
-type Props = {
- params: Promise<{ id: string }>;
-};
-
-
-const NoteDetails = async ({ params }: Props) => {
- const { id } = await params;
- const note = await fetchNoteById(id);
- console.log(note);
-
-
-return (
-    <div>
-        <h1>{note.title}</h1>
-        <p>{note.content}</p>
-    </div>
-  );
-};
- */
 
 import { fetchNoteById } from '@/lib/api';
 import {
